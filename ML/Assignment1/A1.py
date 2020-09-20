1c99eba92d009a747d49748ea9d122202c2d8de9
Aditya Mahaddalkar <adityam1311@gmail.com>
2020-09-20 16:22:47 +0530

    Modified algorithm

--- ML/Assignment1/A1.py
+++ ML/Assignment1/A1.py
@@ -1,64 +1,62 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 
 
 class LinearRegression(object):
-    def __init__(self,file_name):
-        self.file_name=file_name
-        self.dataset=None
-        self.X=None
-        self.Y=None
-        self.b_0=0
-        self.b_1=0
-        
- 
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.dataset = None
+        self.X = None
+        self.Y = None
+        self.b_0 = random.randint(1, 20)
+        self.b_1 = random.randint(1, 20)
+
     def read_dataset(self):
-        self.dataset=pd.read_csv(self.file_name)
+        self.dataset = pd.read_csv(self.file_name)
 
-        self.X=self.dataset.iloc[:,0]
-        self.Y=self.dataset.iloc[:,-1]
-        
-    def calculate_coefficient(self):
-        n=np.size(self.X)
-        
-        mean_x=np.mean(self.X)
-        mean_y=np.mean(self.Y)
-        
-        #calculate cross deviation and deviation about x
-        
-        xy=np.sum(self.Y*self.X)-n*mean_y*mean_x
-        xx=np.sum(self.X*self.X)-n*mean_x*mean_x
-        
-        #coefficients
-        self.b_1=xy/xx
-        self.b_0=mean_y-self.b_1*mean_x
-        
-        
-        
-    def Regression(self):
-        self.calculate_coefficient()
-        print("Equation of line : Y = {} + {}*X".format(self.b_0,self.b_1))
-        
+        self.X = self.dataset.iloc[:, 0]
+        self.Y = self.dataset.iloc[:, -1]
+
+    def calculate_coefficient(self, max_epochs=100000, alpha=0.001):
+
+        y_pred = self.b_0 * self.X + self.b_1
+        n = self.X.shape[0]
+        for epochs in range(max_epochs):
+
+            loss = (1 / 2 * n) * np.sum((self.Y - y_pred) ** 2)
+            self.b_0 -= alpha * (2 / n) *\
+                np.sum(np.dot(self.Y - y_pred, self.X))
+            self.b_1 -= alpha * (2 / n) * np.sum(self.Y - y_pred)
+            y_pred = self.b_0 * self.X + self.b_1
+
+            if epochs % 1000 == 0:
+                print(f"Epoch : {epochs} | Loss : {loss}")
+
+    def Regression(self, max_epochs=100000, alpha=0.001):
+        self.calculate_coefficient(max_epochs, alpha)
+        print("Equation of line : Y = {} + {}*X".format(self.b_0, self.b_1))
+
     def plot_line(self):
-        plt.scatter(self.X, self.Y, color = "m", marker = "o", s = 30) 
-  
-        # predicted response vector 
-        y_pred = self.b_0 + self.b_1*self.X 
+        plt.scatter(self.X, self.Y, color="m", marker="o", s=30)
 
-        # plotting the regression line 
-        plt.plot(self.X, y_pred, color = "g") 
+        # predicted response vector
+        y_pred = self.b_0 + self.b_1*self.X
 
-        # putting labels 
-        plt.xlabel('x') 
-        plt.ylabel('y') 
+        # plotting the regression line
+        plt.plot(self.X, y_pred, color="g")
 
-        # function to show plot 
+        # putting labels
+        plt.xlabel('x')
+        plt.ylabel('y')
+
+        # function to show plot
         plt.show()
-    
-if __name__=='__main__':
-    regressor=LinearRegression('data/sample.csv')
-    regressor.read_dataset()
-    regressor.Regression()
-    regressor.plot_line()
 
+
+if __name__ == '__main__':
+    regressor = LinearRegression('data/sample.csv')
+    regressor.read_dataset()
+    regressor.Regression(10000, 0.00000001)
+    regressor.plot_line()
